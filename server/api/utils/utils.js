5bdd85e9652946321a540db622773ed29e5bf877
Nayara Martovic <nayaramartovic@hotmail.com>
2021-04-24 22:16:10 -0300

    Adicionando uma função de erro ao Utils

--- server/api/utils/utils.js
+++ server/api/utils/utils.js
@@ -1,4 +1,8 @@
 
 exports.objIsEmpty = (obj) => {
     return Object.keys(obj).length == 0;
-}
+};
+
+exports.throwErrorAsPromise = (error) => {
+    return new Promise((res, rej) => rej(error));
+};
